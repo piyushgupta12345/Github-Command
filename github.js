cd1dd713853db05b3e06b319081fe4e4f17df580
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-07-20 16:27:27 +0530

    update

---
+++ github.js
@@ -0,0 +1,75 @@
+console.log("Hello")
+
+// **  Git All command -------------->>>>>>>>>
+
+// git init ------: 
+// Allows you to run any git commands on that folder
+
+// git add file_name -------: 
+// add file in Git's tracking Zone
+
+// git add . -------: 
+// add every file in Git's tracking Zone
+
+// git commit -m "message" -------: 
+// stages the files to get pushed on next "push command"
+
+// git remote --------: 
+// setting address of the location where the changes will be pushed to
+
+// git status --------: 
+// shows the current tracked and untracked files
+
+// git reset --------: 
+// makes all files untracked
+
+// .gitignore --------: 
+// a file used to ignore the files which you don't want to get track of,by Git
+
+// git log --------: 
+// shows the hash code and details about the recent commit
+
+// git clone__ ----:
+// clones/copies the git files from internet to your computer
+
+// git branch  -----:
+// shows all branchs
+
+
+
+
+// commiting and pushing changes on internet
+
+// git add.
+// git commit -m "commit
+// git pull origin master ------: to make sure you're up to date
+// git push origin master ------: pushing changes to the remote
+
+
+
+
+
+// Branching and stuff
+// git branch ------:
+//  show all branch
+
+// git branch B_name ------:
+//  creates the branch with B_name
+
+// git checkout B_name -----:
+//  switches you that to branch
+
+
+
+
+// Merding Branch to Master Branch
+
+// git chackout master ---------:
+// moves to master branch
+
+// git pull origin master ---------:
+// moves to master branch
+
+// git push origin master ---------:
+// pushes the changes
+
